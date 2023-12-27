69a2035900e8ccd8f4bcb25708035def208d276b
Aayushzz <142186606+Aayushzz@users.noreply.github.com>
2023-12-27 18:52:22 -0500

    / fix

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cookieParser());
 app.use(compression())
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/test', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);
 app.get('/', (req, res) => {
